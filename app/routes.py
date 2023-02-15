8a14cf2612836d12f585a7ad4569b4ed580fbfbf
VV <vlad.vysotskiy@gmail.com>
2023-02-15 22:49:16 +0200

    Enabled db connection

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, send_from_directory, request, redirect
 import os
+from flask_mysqldb import MySQL
 from src import tables, datalists
+import config
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app.config.from_object('config')
+
+# Create an instance of the MySQL class
+mysql = MySQL(app)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -31,3 +37,11 @@ def brchoice():
         return render_template('breakerchoice.html', amperages=amperages)
 
     return render_template('breakerchoice.html')
+
+# Use the connection in a view function
+@app.route('/testsql')
+def testsql():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT breaker FROM breakers')
+    data = cur.fetchall()
+    return str(data)
---
+++ config.py
@@ -0,0 +1,6 @@
+# Configure MySQL
+MYSQL_HOST = '10.20.40.5'
+MYSQL_USER = 'ilsdbuser'
+MYSQL_PASSWORD = 'CYFkqKu$TD8P4WeJk!x2'
+MYSQL_DB = 'ils2'
+MYSQL_CURSORCLASS = 'DictCursor'
